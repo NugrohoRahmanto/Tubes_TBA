9a1bf6de249e8873088775ded32464cee34f101a
NugrohoRahmanto <122028111+NugrohoRahmanto@users.noreply.github.com>
2023-06-18 21:15:40 +0700

    Add files via upload

---
+++ scanner.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("D:/tubes/tba/scanner_example.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		token := strings.Fields(line)
+		for i := 0; i < len(token); i++ {
+			fmt.Println(token[i])
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
